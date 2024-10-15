e7ff2b46a0d60005a61265fc9cbbaffbc028beb1
Michael Waterworth <glastohacks@gmail.com>
2024-10-15 12:50:19 +0100

    Add google login button to homepage

--- frontend/src/components/Hello.vue
+++ frontend/src/components/Hello.vue
@@ -23,17 +23,17 @@
           <v-card
             class="py-4"
             color="surface-variant"
-            prepend-icon="mdi-rocket-launch-outline"
+            prepend-icon="mdi-earth"
             rounded="lg"
             variant="outlined"
           >
             <template #title>
-              <h2 class="text-h5 font-weight-bold">Get started</h2>
+              <h2 class="text-h5 font-weight-bold">Caves</h2>
             </template>
 
             <template #subtitle>
               <div class="text-subtitle-1">
-                Browse caves, systems, and trips
+                Browse caves and cave systems
               </div>
             </template>
 
@@ -47,10 +47,70 @@
           </v-card>
         </v-col>
       </v-row>
+
+      <v-row>
+        <v-col cols="12">
+          <v-card
+            class="py-4"
+            color="surface-variant"
+            prepend-icon="mdi-notebook-outline"
+            rounded="lg"
+            variant="outlined"
+          >
+            <template #title>
+              <h2 class="text-h5 font-weight-bold">Trips</h2>
+            </template>
+
+            <template #subtitle>
+              <div class="text-subtitle-1">
+                Log your trips and view public trips
+              </div>
+            </template>
+
+            <v-overlay
+              opacity=".12"
+              scrim="primary"
+              contained
+              model-value
+              persistent
+            />
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <div id="loginWithGoogle"></div>
+          <div id="g_id_onload"
+            data-client_id="1062453848042-08ktfa8rv4pe786okrd2dc0k3ls4hksp.apps.googleusercontent.com"
+            data-login_uri="http://localhost:3000/api/google/redirect"
+            data-auto_prompt="true">
+          </div>
+          <div class="g_id_signin"
+            data-type="standard"
+            data-size="large"
+            data-theme="outline"
+            data-text="sign_in_with"
+            data-shape="rectangular"
+            data-logo_alignment="left">
+          </div>
+        </v-col>
+      </v-row>
     </v-responsive>
   </v-container>
 </template>
 
 <script setup>
-  //
+import { onMounted } from 'vue';
+
+  onMounted(() => {
+    google.accounts.id.initialize({
+      client_id: "1062453848042-08ktfa8rv4pe786okrd2dc0k3ls4hksp.apps.googleusercontent.com"
+      // callback: handleCredentialResponse
+    });
+    google.accounts.id.renderButton(
+      document.getElementById("loginWithGoogle"),
+      { theme: "outline", size: "large" }  // customization attributes
+    );
+    // google.accounts.id.prompt(); // also display the One Tap dialog
+  });
 </script>
