06f24dbb69729216a1fbe4f787c6d4a64d673134
Michael Waterworth <glastohacks@gmail.com>
2024-11-01 11:42:14 +0000

    Limit POST/PUT/DELETE to is_admin

---
+++ app/Http/Middleware/ApiIsAdmin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiIsAdmin
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(! $request->user() || ! $request->user()->is_admin)
+        {
+            return response()->json(['error' => 'User is not authorised to perform that action'], 401);
+        }
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\ApiIsAuthenticated;
+use App\Http\Middleware\ApiIsAdmin;
 use App\Http\Resources\UserDetailResource;
 
 Route::get('/caves', [App\Http\Controllers\CaveController::class, 'index']);
-Route::post('/caves', [App\Http\Controllers\CaveController::class, 'store']);
+Route::post('/caves', [App\Http\Controllers\CaveController::class, 'store'])->middleware(ApiIsAdmin::class);
 Route::get('/caves/{cave}', [App\Http\Controllers\CaveController::class, 'show']);
-Route::put('/caves/{cave}', [App\Http\Controllers\CaveController::class, 'update']);
+Route::put('/caves/{cave}', [App\Http\Controllers\CaveController::class, 'update'])->middleware(ApiIsAdmin::class);
 
 Route::get('/trips', [App\Http\Controllers\TripController::class, 'index']);
 Route::post('/trips', [App\Http\Controllers\TripController::class, 'store']);
