4304563a72e12864aa4a6e965c55127cbd1e2a7b
Michael Waterworth <glastohacks@gmail.com>
2025-03-30 11:49:41 +0100

    Add geojson to map layer

--- frontend/src/components/CaveListMap.vue
+++ frontend/src/components/CaveListMap.vue
@@ -8,24 +8,73 @@
             :zoom="zoom"
             height="500px"
           >
-            <mgl-marker :coordinates="lnglat" color="#cc0000" />
-            <mgl-navigation-control />
+          <mgl-geo-json-source
+            source-id="geojson"
+            :data="geojsonSource.data"
+          >
+            <mgl-symbol-layer
+              layer-id="geojson"
+              :layout="layout"
+              :paint="paint"
+            />
+          </mgl-geo-json-source>
+          <mgl-navigation-control />
           </mgl-map>
         </v-card-text>
       </v-card>
 </template>
 
 <script setup>
+  import { useCaveStore } from '@/stores/caves'
+  const caveStore = useCaveStore()
+  import { computed } from 'vue';
+  
+
 import {
   MglMap,
   MglNavigationControl,
-  MglMarker,
+  MglGeoJsonSource,
+  MglSymbolLayer,
 } from '@indoorequal/vue-maplibre-gl';
 
 const style = 'https://api.maptiler.com/maps/outdoor-v2/style.json?key=0gGMv4po9Mjrpd64A528';
-const zoom = 11;
+const zoom = 9;
 
 const lnglat = [-2.609, 51.501]
+
+const geojsonSource = computed(()=>{
+  return {
+    data: {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "properties": {},
+          "geometry": {
+            "coordinates": [
+              -2.6837168915738516,
+              51.236467389561284
+            ],
+            "type": "Point"
+          }
+        }
+      ]
+    }
+  }
+  // caveStore.caves.map()
+})
+
+
+const layout = {
+  // 'line-join': 'round',
+  // 'line-cap' : 'round'
+};
+
+const paint = {
+  // 'line-color': '#FF0000',
+  // 'line-width': 8
+};
+
 
 </script>
 
