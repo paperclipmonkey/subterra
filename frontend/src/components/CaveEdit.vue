efc17f15cdef03a2de523b206b6d0342629f85b2
Michael Waterworth <glastohacks@gmail.com>
2025-04-04 13:14:22 +0100

    Add support for editing the entrance location on a map

--- frontend/src/components/CaveEdit.vue
+++ frontend/src/components/CaveEdit.vue
@@ -21,6 +21,17 @@
               <v-textarea v-model="cave.description" label="Description" required></v-textarea>
               <v-text-field v-model="cave.location_name" label="Location Name" required></v-text-field>
               <v-text-field v-model="cave.location_country" label="Country" required></v-text-field>
+
+              <mgl-map
+                :map-style="style"
+                :center="initmapCentre"
+                :zoom="zoom"
+                height="350px"
+              >
+                <mgl-marker :draggable="true" v-model:coordinates="coordinates" color="#cc0000" />
+                <mgl-navigation-control />
+                <MglGeolocateControl/>
+              </mgl-map>
               <v-text-field v-model="cave.location_lat" label="Latitude" required></v-text-field>
               <v-text-field v-model="cave.location_lng" label="Longitude" required></v-text-field>
               <v-file-input
@@ -108,90 +119,124 @@
 
 <script setup>
 import VueMarkdown from 'vue-markdown-render'
-import { watch } from "vue"
+import { computed, watch } from "vue"
 import { useRoute, useRouter } from "vue-router"
 import { convertFileToBase64 } from '@/utilities.js'
+
+import {
+  MglMap,
+  MglNavigationControl,
+  MglMarker,
+  MglGeolocateControl,
+} from '@indoorequal/vue-maplibre-gl';
+import { LngLat } from 'maplibre-gl';
+
+const style = 'https://api.maptiler.com/maps/outdoor-v2/style.json?key=0gGMv4po9Mjrpd64A528';
+const zoom = 16;
+
 
 const router = useRouter()
 const route = useRoute()
 
 const selectedTags = ref({})
 
-  const cave = ref({
+const cave = ref({
+  name: '',
+  description: '',
+  hero_image: '',
+  entrance_image: '',
+  system: {
     name: '',
     description: '',
-    hero_image: '',
-    entrance_image: '',
-    system: {
-      name: '',
-      description: '',
-      length: '',
-      vertical_range: '',
-      caves: []
-    },
-    location: {
-      name: '',
-      country: '',
-      lat: 0,
-      lng: 0,
-    },
-    trips: []
-  })
+    length: '',
+    vertical_range: '',
+    caves: []
+  },
+  location: {
+    name: '',
+    country: '',
+    lat: 0,
+    lng: 0,
+  },
+  trips: [],
+  location_lat: 0,
+  location_lng: 0,
+})
 
-  const tagsAvailable = ref({});
+const tagsAvailable = ref({});
+
+const initmapCentre = ref([0,0])
 
 
-  const fetchCave = async () => {
-    const response = await fetch(`/api/caves/${route.params.id}`)
-    cave.value = (await response.json()).data
+const coordinates = ref(LngLat.convert([cave.value.location_lng, cave.value.location_lat]));
 
-    selectedTags.value = cave.value.tags.reduce((acc, tag) => {
-      if(tag.type !== 'cave') { // Only show cave tags
-        return acc
-      }
-      if (!acc[tag.category]) {
-        acc[tag.category] = []
-      }
-      acc[tag.category].push(tag.tag)
+watch(coordinates, (newCoordinates) => {
+  cave.value.location_lng = roundToMetre(newCoordinates['lng']);
+  cave.value.location_lat = roundToMetre(newCoordinates['lat']);
+});
+
+// https://en.wikipedia.org/wiki/Decimal_degrees
+const roundToMetre = (val) => {
+  return Math.round(val * 100000) / 100000 // round to 5 decimal places
+}
+
+const fetchCave = async () => {
+  const response = await fetch(`/api/caves/${route.params.id}`)
+  cave.value = (await response.json()).data
+
+  coordinates.value = LngLat.convert([cave.value.location_lng, cave.value.location_lat])
+  initmapCentre.value = [cave.value.location_lng, cave.value.location_lat]
+
+  selectedTags.value = cave.value.tags.reduce((acc, tag) => {
+    if(tag.type !== 'cave') { // Only show cave tags
       return acc
-    }, {})
+    }
+    if (!acc[tag.category]) {
+      acc[tag.category] = []
+    }
+    acc[tag.category].push(tag.tag)
+    return acc
+  }, {})
 
-    const tagsresponse = await fetch('/api/tags');
-    tagsAvailable.value = (await tagsresponse.json())
-    
-    // TODO: remove tags that aren't related to a cave
+  const tagsresponse = await fetch('/api/tags');
+  tagsAvailable.value = (await tagsresponse.json())
+  
+  // TODO: remove tags that aren't related to a cave
+}
+
+const saveCave = async () => {
+  // Re-add tags to cave
+  cave.value.tags = Object.entries(selectedTags.value).reduce((acc, [category, tags]) => {
+    return acc.concat(tags.map(tag => ({ category, tag })))
+  }, [])
+
+  if(cave.value.hero_image instanceof File) {
+    cave.value.hero_image = await convertFileToBase64(cave.value.hero_image);
   }
 
-  const saveCave = async () => {
-    // Re-add tags to cave
-    cave.value.tags = Object.entries(selectedTags.value).reduce((acc, [category, tags]) => {
-      return acc.concat(tags.map(tag => ({ category, tag })))
-    }, [])
-
-    if(cave.value.hero_image instanceof File) {
-      cave.value.hero_image = await convertFileToBase64(cave.value.hero_image);
-    }
-
-    if(cave.value.entrance_image instanceof File) {
-      cave.value.entrance_image = await convertFileToBase64(cave.value.entrance_image);
-    }
-
-    const response = await fetch(`/api/caves/${route.params.id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(cave.value),
-    })
-    if (response.ok) {
-      router.push({ name: '/cave/[id]', params: { id: cave.value.slug } })
-    }
+  if(cave.value.entrance_image instanceof File) {
+    cave.value.entrance_image = await convertFileToBase64(cave.value.entrance_image);
   }
 
-  onMounted(fetchCave)
+  const response = await fetch(`/api/caves/${route.params.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(cave.value),
+  })
+  if (response.ok) {
+    router.push({ name: '/cave/[id]', params: { id: cave.value.slug } })
+  }
+}
 
-  watch(
-    () => route.fullPath,
-    fetchCave
-  )
+onMounted(fetchCave)
+
+watch(
+  () => route.fullPath,
+  fetchCave
+)
 </script>
+<style lang="scss">
+@import "maplibre-gl/dist/maplibre-gl.css";
+</style>
