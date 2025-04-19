e725a361f022e7ede29e2ec392d41175002a1480
Michael Waterworth <glastohacks@gmail.com>
2025-04-19 22:58:47 +0100

    Update UserCreated to fire an event

---
+++ app/Listeners/SendUserCreatedSlackAlert.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserCreated;
+use Spatie\SlackAlerts\Facades\SlackAlert;
+
+class SendUserCreatedSlackAlert
+{
+    public function handle(UserCreated $event)
+    {
+        $user = $event->user;
+        SlackAlert::to('signups')->message("A new user has signed up {$user->name} with email: {$user->email}");
+    }
+}
