da40c8aa5be8379ea4c5a8abf35243f20d1eecbf
Michael Waterworth <glastohacks@gmail.com>
2025-04-19 12:40:22 +0100

    Add tests for getting caves

--- database/factories/CaveSystemFactory.php
+++ database/factories/CaveSystemFactory.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
-class CaveFactory extends Factory
+class CaveSystemFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -19,14 +19,10 @@ class CaveFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'description' => fake()->sentence(30),
-            'location_name' => fake()->city(),
-            'location_country' => fake()->country(),
-            'location_lat' => fake()->randomFloat(3,-180,180),
-            'location_lng' => fake()->randomFloat(3,-180,180),
-            'location_alt' => fake()->randomDigitNotNull(),
             'slug' => fake()->slug(),
-            'cave_system_id' => CaveSystem::factory()->create()->id,
+            'description' => fake()->sentence(30),
+            'length' => fake()->randomDigitNotNull(),
+            'vertical_range' => fake()->randomDigitNotNull(),
         ];
     }
 
