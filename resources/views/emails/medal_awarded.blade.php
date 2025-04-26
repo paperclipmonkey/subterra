92362bab597d0a27155574b83d1f39ed297700a0
Michael Waterworth <glastohacks@gmail.com>
2025-04-26 16:13:36 +0100

    Update medal email to show image

--- resources/views/emails/medal_awarded.blade.php
+++ resources/views/emails/medal_awarded.blade.php
@@ -5,12 +5,13 @@
 <p>Congratulations! You have earned a new medal:</p>
 <p>
   @if($medal->image_path)
-    <img src="{{ Storage::disk('medals')->url($medal->image_path) }}" alt="{{ $medal->name }}" style="height:64px;vertical-align:middle;margin-right:12px;border-radius:8px;background:#fff;box-shadow:0 2px 8px #eee;" />
+    <img src="{{ Storage::disk('medals')->url(str_replace('.svg', '.png', $medal->image_path)) }}" alt="{{ $medal->name }}" style="height:64px;vertical-align:middle;margin-right:12px;border-radius:8px;background:#fff;box-shadow:0 2px 8px #eee;" />
   @endif
   <strong>{{ $medal->name }}</strong>
 </p>
 @if($medal->description)
 <p>{{ $medal->description }}</p>
 @endif
+<p>To view your medals, visit your <a href="{{ config('app.url') }}/profile/{{ $user->id }}">profile</a>.</p>
 <p>Keep up the great work!</p>
 @endsection
