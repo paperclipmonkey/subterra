5b30f6d8c43bbfcc4441fefed40d32fbd7d09644
Michael Waterworth <glastohacks@gmail.com>
2025-05-14 15:33:05 +0100

    Add a cave system with multiple entrances to seed data

--- database/seeders/CaveSeeder.php
+++ database/seeders/CaveSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Cave;
+use App\Models\CaveSystem;
 use Illuminate\Database\Seeder;
 
 class CaveSeeder extends Seeder
@@ -13,5 +14,17 @@ class CaveSeeder extends Seeder
     public function run(): void
     {
         Cave::factory()->count(3)->create();
+
+        // Create a cave system with multiple caves
+        $caveSystem = CaveSystem::factory()->create();
+
+        // Create multiple caves within the cave system
+        for ($i = 0; $i < 3; $i++) {
+            Cave::factory()->create([
+                'cave_system_id' => $caveSystem->id,
+                'name' => 'Cave ' . ($i + 1),
+                'description' => 'Description for Cave ' . ($i + 1),
+            ]);
+        }
     }
 }
