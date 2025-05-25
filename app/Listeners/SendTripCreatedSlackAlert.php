02587ef1eef59f887af80cd58428512b27d1263c
Michael Waterworth <glastohacks@gmail.com>
2025-05-25 21:21:04 +0100

    Add Try around Slack notifications as they're not mission critical

--- app/Listeners/SendTripCreatedSlackAlert.php
+++ app/Listeners/SendTripCreatedSlackAlert.php
@@ -4,6 +4,7 @@ namespace App\Listeners;
 
 use App\Events\TripCreated;
 use Spatie\SlackAlerts\Facades\SlackAlert;
+use Illuminate\Support\Facades\Log;
 
 class SendTripCreatedSlackAlert
 {
@@ -11,6 +12,10 @@ class SendTripCreatedSlackAlert
     {
         $trip = $event->trip;
         $user = $event->user;
-        SlackAlert::to('trips')->message("A new trip has been created: <https://subterra.world/trip/{$trip->id}|{$trip->name}> to {$trip->entrance->name} by {$user->name}");
+        try {
+            SlackAlert::to('trips')->message("A new trip has been created: <https://subterra.world/trip/{$trip->id}|{$trip->name}> to {$trip->entrance->name} by {$user->name}");
+        } catch (\Exception $e) {
+            Log::error('Failed to send Slack alert: ' . $e->getMessage());
+        }
     }
 }
--- app/Listeners/SendUserCreatedSlackAlert.php
+++ app/Listeners/SendUserCreatedSlackAlert.php
@@ -4,12 +4,17 @@ namespace App\Listeners;
 
 use App\Events\UserCreated;
 use Spatie\SlackAlerts\Facades\SlackAlert;
+use Illuminate\Support\Facades\Log;
 
 class SendUserCreatedSlackAlert
 {
     public function handle(UserCreated $event)
     {
         $user = $event->user;
-        SlackAlert::to('signups')->message("A new user has signed up: {$user->name} <https://subterra.world/admin/users|View User> with email: {$user->email}");
+        try {
+            SlackAlert::to('signups')->message("A new user has signed up: {$user->name} <https://subterra.world/admin/users|View User> with email: {$user->email}");
+        } catch (\Exception $e) {
+            Log::error('Failed to send Slack alert: ' . $e->getMessage());
+        }
     }
 }
