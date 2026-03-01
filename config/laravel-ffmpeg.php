8e9d1f50ec67317360cb1b5da232689341cb3ef7
Michael Waterworth <glastohacks@gmail.com>
2026-03-01 23:31:48 +0000

    Optimise performance

--- config/laravel-ffmpeg.php
+++ config/laravel-ffmpeg.php
@@ -4,14 +4,14 @@ return [
     'ffmpeg' => [
         'binaries' => env('FFMPEG_BINARIES', '/usr/bin/ffmpeg'),
 
-        'threads' => 12,   // set to false to disable the default 'threads' filter
+        'threads' => 1,   // set to false to disable the default 'threads' filter
     ],
 
     'ffprobe' => [
         'binaries' => env('FFPROBE_BINARIES', '/usr/bin/ffprobe'),
     ],
 
-    'timeout' => 3600,
+    'timeout' => 600,
 
     'log_channel' => env('LOG_CHANNEL', 'stack'),   // set to false to completely disable logging
 
