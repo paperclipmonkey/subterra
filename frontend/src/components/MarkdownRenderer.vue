3d2e575169bdf2046062333097638aa2e36128d6
Michael Waterworth <glastohacks@gmail.com>
2026-05-31 11:56:28 +0100

    Remove duplicate html

--- frontend/src/components/MarkdownRenderer.vue
+++ frontend/src/components/MarkdownRenderer.vue
@@ -15,19 +15,6 @@
       </v-card>
     </v-dialog>
   </div>
-
-  <v-dialog v-model="showDiagramModal" max-width="95vw">
-    <v-card class="rounded-lg overflow-auto">
-      <v-card-text class="pa-6 d-flex justify-center">
-        <!-- eslint-disable-next-line vue/no-v-html -->
-        <div v-html="diagramSvg" />
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer />
-        <v-btn variant="text" @click="showDiagramModal = false">Close</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
 </template>
 
 <script>
